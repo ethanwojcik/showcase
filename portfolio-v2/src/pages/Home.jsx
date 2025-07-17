1f7c3074088ded33fcc7f59a483e0ecd32627ba9
ethanwojcik <ewoj@umich.edu>
2025-07-17 18:18:27 -0400

    testing

---
+++ portfolio-v2/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+
+import { ProjectsSection } from "../components/ProjectsSection";
+export const  Home =() => {
+  return (
+    <div className="bg-background text-foreground min-h-screen overflow-x-hidden">
+      
+      <main> 
+        <ProjectsSection />
+      </main>
+      
+    </div>
+    
+  );
+}
