5b96ade5a893e44d497c6a3f78830e0aef92267a
ethanwojcik <ewoj@umich.edu>
2025-07-18 16:01:01 -0400

    adding about me

--- portfolio-v3/src/components/ProjectsSection.jsx
+++ portfolio-v3/src/components/ProjectsSection.jsx
@@ -3,8 +3,8 @@ const projects = [
         id:1,
         title: "Placeholder",
         description: "Desc",
-        image: "DSC_9168.JPEG",
-        tags: ["React","TailwindCSS","Supabase"],
+        image: "../../public/vite.svg",
+        tags: ["React","TailwindCSS",],
         demoUrl: "#",
         githubUrl: "#"
     },
@@ -12,7 +12,7 @@ const projects = [
         id:2,
         title: "Placeholder_2",
         description: "Desc",
-        image: "headshot_2.jpg",
+        image: "../../public/vite.svg",
         tags: ["React",],
         demoUrl: "#",
         githubUrl: "#"
@@ -21,30 +21,38 @@ const projects = [
 export const ProjectsSection = () => {
     return (<section id="projects" className="py-24 px-4 relative">
             <div className="container mx-auto max-w-5xl">   
-                <h2 className="text-3xl md:text-4xl font-bold mb-4 text-center"> Featured  <span className="text-primary"> Projects
+                <h2 className="text-3xl md:text-4xl font-bold mb-4 text-center"> Featured  
+                    <span className="text-primary"> 
+                        {" "}Projects
                     </span>
-                    </h2>
+                </h2>
                 <p className= "text-center text-muted-forground mb-12 max-w-2xl mx-auto">
                     Sample Text
                 </p>
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8"></div>
-                {projects.map((project,key) => (
-                    <div key={key} className="group bg-card rounded-lg overflow-hidden shadow-xs card-hover">
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+                
+                    {projects.map((project,key) => (
+                        <div key={key} className="group bg-card rounded-lg overflow-hidden shadow-xs card-hover">
 
-                        <div className="h-48 overflow-hidden">
-                            <img src={project.image} alt={project.title} className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-110"/>
-                        </div>
-                        <div className="p-6">
-                            <div className="flex flex-wrap gap-2 mb-4">
-                                {project.tags.map((tag)=>  (
-                                    <span key={tag} className="px-2 py-1 text-xs font-medium rounded-full bg-secondary text-secondary-foreground"> {tag}</span>
-                                ))}
+                            <div className="h-48 overflow-hidden">
+                                <img src={project.image} alt={project.title} className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-110"/>
                             </div>
-                        </div>
+                            <div className="p-6">
+                                <div className="flex flex-wrap gap-2 mb-4">
+                                    {project.tags.map((tag)=>  (
+                                        <span key={tag} className="px-2 py-1 text-xs font-medium border rounded-full bg-secondary text-secondary-foreground"> {tag}</span>
+                                    ))}
+                                </div>
+                            </div>
 
+                        
+                        </div>
                     
-                    </div>
-                ))}
+
+                    ))}
+                
+                </div>
+                
             </div>
         </section>);
 };
--- portfolio-v3/src/pages/Home.jsx
+++ portfolio-v3/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 
 import { ProjectsSection } from "../components/ProjectsSection";
 
+import { AboutMe } from "../components/AboutMe";
+
 export const Home = () => {
   return (
     <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
 
       <main>
-
+        <AboutMe/>
         <ProjectsSection />
 
       </main>
