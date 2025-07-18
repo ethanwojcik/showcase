d7bc8d826d29317f5df9c5971c23685fe6557067
ethanwojcik <ewoj@umich.edu>
2025-07-18 18:36:28 -0400

    added theme toggle

--- portfolio-v3/src/pages/Home.jsx
+++ portfolio-v3/src/pages/Home.jsx
@@ -1,12 +1,12 @@
 
 import { ProjectsSection } from "../components/ProjectsSection";
-
+import { ThemeToggle  } from "../components/ThemeToggle";
 import { AboutMe } from "../components/AboutMe";
 
 export const Home = () => {
   return (
     <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
-
+        <ThemeToggle/>
       <main>
         <AboutMe/>
         <ProjectsSection />
