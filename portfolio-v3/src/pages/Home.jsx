cf35a3fbb2e5b4068c5bedb2c4ee6ba1953664dc
ethanwojcik <ewoj@umich.edu>
2025-07-19 14:06:57 -0400

    .

--- portfolio-v3/src/pages/Home.jsx
+++ portfolio-v3/src/pages/Home.jsx
@@ -3,13 +3,17 @@ import { ProjectsSection } from "../components/ProjectsSection";
 import { ThemeToggle  } from "../components/ThemeToggle";
 import { AboutMe } from "../components/AboutMe";
 import { StarBackground } from "../components/StarBackground";
+import { Navbar } from "../components/Navbar";
+import { Introduction } from "../components/Introduction";
 
 export const Home = () => {
   return (
     <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
         <ThemeToggle/>
         <StarBackground/>
+        <Navbar/>
       <main>
+        <Introduction/>
         <AboutMe/>
         <ProjectsSection />
 
