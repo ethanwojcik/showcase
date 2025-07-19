d7d5de12516483aadf36133907c337e3147d2030
ethanwojcik <ewoj@umich.edu>
2025-07-18 21:52:10 -0400

    animations

--- portfolio-v3/src/pages/Home.jsx
+++ portfolio-v3/src/pages/Home.jsx
@@ -2,11 +2,13 @@
 import { ProjectsSection } from "../components/ProjectsSection";
 import { ThemeToggle  } from "../components/ThemeToggle";
 import { AboutMe } from "../components/AboutMe";
+import { StarBackground } from "../components/StarBackground";
 
 export const Home = () => {
   return (
     <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
         <ThemeToggle/>
+        <StarBackground/>
       <main>
         <AboutMe/>
         <ProjectsSection />
